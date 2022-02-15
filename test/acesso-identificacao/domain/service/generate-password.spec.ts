9021859880ad9367410fe6436f4056cb3115c313
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-02-14 21:50:21 -0300

    refactor: remover testes não utilizados

--- test/acesso-identificacao/domain/service/generate-password.spec.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt'
-import GeneratePassword from 'src/acesso-identificacao/domain/service/generate-password';
-
-
-test("Should create a new password", async () => {
-  const password = await GeneratePassword.generate('dummy');
-  const compare = await bcrypt.compare('dummy', password)
-  expect(compare).toBeTruthy()
-});
