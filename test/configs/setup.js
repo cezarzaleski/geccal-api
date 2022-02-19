7d7d7beac0d8d546c831db743256b26e791f1a50
Cezar Gabriel Heinen Zaleski <cezar.zaleski@creditas.com>
2022-02-18 22:09:31 -0300

    chore: require dotenv setup jest

--- test/configs/setup.js
+++ test/configs/setup.js
@@ -1,9 +1,4 @@
 require('iconv-lite').encodingExists('foo')
+require('dotenv/config')
 
-module.exports = async () => {
-  process.env.MYSQL_HOST='us-cdbr-iron-east-05.cleardb.net'
-  process.env.MYSQL_PORT=3306
-  process.env.MYSQL_USER='b5351039e7e463'
-  process.env.MYSQL_PASSWORD='021fafa0'
-  process.env.MYSQL_DATABASE='heroku_fe076c2ee5de15b'
-}
+module.exports = async () => {}
